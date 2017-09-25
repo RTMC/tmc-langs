90d6de81f0a2aff1d3b955bc8980a67404cda921
samuvait <samu.vaittinen@helsinki.fi>
2017-09-25 22:19:13 +0300

    fixed checkstyle

--- tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/RPlugin.java
+++ tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/RPlugin.java
@@ -21,7 +21,8 @@ import java.util.Locale;
 
 public class RPlugin extends AbstractLanguagePlugin{
 
-    // Various static final Path-variables for filepaths to various folders and files in a R exercise project here
+    // Various static final Path-variables for filepaths
+    // to various folders and files in a R exercise project here
     private static final Path R_FOLDER_PATH = Paths.get("R");
     private static final Path TEST_FOLDER_PATH = Paths.get("tests");
     private static final Path TESTTHAT_FOLDER_PATH = Paths.get("testthat");
@@ -32,7 +33,8 @@ public class RPlugin extends AbstractLanguagePlugin{
 
 
 
-    // Various static final String-variables for error messages related to parsing and running R tests here
+    // Various static final String-variables for
+    // error messages related to parsing and running R tests here
 
 
     private static Logger log = LoggerFactory.getLogger(RPlugin.class);
@@ -52,17 +54,20 @@ public class RPlugin extends AbstractLanguagePlugin{
                 || Files.exists(path.resolve(DESCRIPTION_PATH))
                 || Files.exists(path.resolve(RHISTORY_PATH))
                 || Files.exists(path.resolve(TMC_FOLDER_PATH).resolve(RESULT_R_PATH));
-
         /*
-        R folder contains the actual R files used in the project/package. It is automatically included when creating a
+        R folder contains the actual R files used in the
+        project/package. It is automatically included when creating a
         R package but now when making a regular project in RStudio.
 
-        test/testthat folder contains the unit testing files which use the testThat library for the R project.
+        test/testthat folder contains the unit testing
+        files which use the testThat library for the R project.
 
-        DESCRIPTION file contains package information. Included automatically when making a new package, but not
+        DESCRIPTION file contains package information.
+        Included automatically when making a new package, but not
         included when making a regular project in RStudio.
 
-        .RHistory file contains the history of executed code on the R terminal. Generated after running code on the R
+        .RHistory file contains the history of executed code on
+        the R terminal. Generated after running code on the R
         terminal for the first time.
 
         tmc/result.R contains the call to tmcRtestrunner's runTests function.
