feeb151b3c5e64a117eab7a59abd64e9d01913f0
Janne Hyttinen <hyttijan@cs.helsinki.fi>
2017-10-02 15:21:42 +0300

    had to increase code coverage

--- tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/RPlugin.java
+++ tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/RPlugin.java
@@ -137,7 +137,7 @@ public class RPlugin extends AbstractLanguagePlugin{
         if (SystemUtils.IS_OS_WINDOWS) {
             rscr = new String[] {"Rscript", "-e"};
             command = new String[] {"\"library(tmcRtestrunner);"
-                                    + "getAvailablePoints(\"$PWD\")\""};
+                                    + "get_available_points(\"$PWD\")\""};
         } else {
             rscr = new String[] {"bash"};
             command = new String[] {Paths.get("").toAbsolutePath().toString() 
